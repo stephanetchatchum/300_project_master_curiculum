60ac85a46d873a37b88700e30c36f70b939d964f
stephanetchatchum@gmail.com <stephanetchatchum@gmail.com>
2026-06-02 20:14:56 +0200

    adding the companion guide

--- Project_13_Student_Grade_Manager/main.py
+++ Project_13_Student_Grade_Manager/main.py
@@ -1,3 +1,4 @@
+import statistics as s
 class student:
     def __init__(self, name, student_id, grades=None):
         self.name = name 
@@ -5,22 +6,52 @@ class student:
         self.grades = grades if grades is not None else []
 
     def add_grade(self,  grade):
-        pass
+        try:
+            numeric_grade = float(grade)
+            if 0 <= numeric_grade <= 100:
+                self.grades.append(numeric_grade)
+                return True
+            print("❌ Error: Grade must be between 0 and 100.")
+            return False
+        except ValueError:
+            print("❌ Error: Grade must be a valid number.")
+            return False
 
     def get_average(self):
-        pass
+        if not self.grades:
+            return 0.0
+        return s.mean(self.grades)
 
     def get_letter_grade(self):
-        pass
+        letter_grade = None
+        if self.grades<=50 and self.grades>40:
+            letter_grade = "E"
+        elif self.grades<=60 and self.grades>50:
+            letter_grade = "D"
+        elif self.grades<=70 and self.grades>60:
+            letter_grade = "C"
+        elif self.grades<=80 and self.grades>70:
+            letter_grade = "B"
+        elif self.grades<=90 and self.grades>80:
+            letter_grade = "A"
+        else:
+            letter_grade = "F"
+
+        return letter_grade
     
-    def to_dict(self):
-        pass
+    # def to_dict(self):
+    #     pass
 
     def __str__(self):
-        pass
+        return f"{self.name} ({self.student_id}): {len(self.grades)} grades"
 
+def menu():
+    print("Welcome to Student grade Manager!\n")
+    print("1. ")
 def main():
-    pass
+    student = {}
+    
+
 
 if __name__ == "__main__":
     main()
